5a488d93f3cc836936db8dbdb168bbdb41054ac4
rayadinaveen <106173513+rayadinaveen@users.noreply.github.com>
2022-09-07 17:56:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "This is the amazing app EVER, speaking at wdi."
+    return "Hello World."
  
 
 @app.route("/healthz")
